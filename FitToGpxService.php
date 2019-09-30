c4c0e016932cfeab97aa7b4eb012e3f48abdc8a1
kzmr <USER_INFO@users.noreply.github.com>
2019-09-30 11:17:10 +0900

    add file

---
+++ FitToGpxService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * FitToGpxService
+ */
+class FitToGpxService
+{
+    public function __construct(){
+        error_log("pass FitToGpxService __construct");
+    }
+
+    public static function convert()
+    {
+        error_log("pass FitToGpxService convert");
+    }
+}
