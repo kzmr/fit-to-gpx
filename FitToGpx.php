d15c34ca9eb4e0276d2d07fa4704709732eb6265
kzmr <USER_INFO@users.noreply.github.com>
2019-09-30 16:49:12 +0900

    add logic

--- FitToGpx.php
+++ FitToGpx.php
@@ -1,15 +1,33 @@
 <?php
+
+require __DIR__.'/vendor/autoload.php';
+
 /**
  * FitToGpx
  */
 class FitToGpx
 {
-    public function __construct(){
-        error_log("pass FitToGpx __construct");
+
+    protected $pFFA;
+
+    public function __construct($input_fit_file){
+        $this->pFFA = new phpFITFileAnalysis($input_fit_file);
     }
 
-    public static function convert()
+    public function saveAsGpx($output_gpx_file)
     {
-        error_log("pass FitToGpx convert");
+        $rootNode = new \SimpleXMLElement('<?xml version="1.0" encoding="UTF-8" standalone="no"?><gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1"></gpx>');
+        $trkNode = $rootNode->addChild('trk');
+        $trksegNode = $trkNode->addChild('trkseg');
+
+        foreach ($this->pFFA->data_mesgs['record']['timestamp'] as $timestamp) {
+            $trkptNode = $trksegNode->addChild('trkpt');
+            $trkptNode->addAttribute('lat', $this->pFFA->data_mesgs['record']['position_lat'][$timestamp]);
+            $trkptNode->addAttribute('lon', $this->pFFA->data_mesgs['record']['position_long'][$timestamp]);
+            $trkptNode->addChild('ele', $this->pFFA->data_mesgs['record']['altitude'][$timestamp]);
+            $trkptNode->addChild('time', date('Y-m-d\TH:i:s.000\Z', $timestamp));
+        }
+
+        file_put_contents($output_gpx_file, $rootNode->asXML());
     }
 }
