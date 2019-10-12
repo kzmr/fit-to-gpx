357082e52137f4e93e41fc6a5de782a8811c5eee
kzmr <USER_INFO@users.noreply.github.com>
2019-10-12 10:52:00 +0900

    error handling

--- FitToGpx.php
+++ FitToGpx.php
@@ -5,9 +5,22 @@
 class FitToGpx
 {
     protected $pFFA;
+    protected $errorMessage;
 
     public function __construct($input_fit_file){
-        $this->pFFA = new adriangibbons\phpFITFileAnalysis($input_fit_file);
+        try {
+            $this->pFFA = new adriangibbons\phpFITFileAnalysis($input_fit_file);
+        } catch ( Exception $ex ) {
+            $this->errorMessage = $ex->getMessage();
+        }
+    }
+
+    public function getError()
+    {
+        if ($this->errorMessage) {
+            return $this->errorMessage;
+        }
+        return null;
     }
 
     public function saveAsGpx($output_gpx_file)
@@ -16,14 +29,17 @@ class FitToGpx
         $trkNode = $rootNode->addChild('trk');
         $trksegNode = $trkNode->addChild('trkseg');
 
-        foreach ($this->pFFA->data_mesgs['record']['timestamp'] as $timestamp) {
-            $trkptNode = $trksegNode->addChild('trkpt');
-            $trkptNode->addAttribute('lat', $this->pFFA->data_mesgs['record']['position_lat'][$timestamp]);
-            $trkptNode->addAttribute('lon', $this->pFFA->data_mesgs['record']['position_long'][$timestamp]);
-            $trkptNode->addChild('ele', $this->pFFA->data_mesgs['record']['altitude'][$timestamp]);
-            $trkptNode->addChild('time', date('Y-m-d\TH:i:s.000\Z', $timestamp));
+        try {
+            foreach ($this->pFFA->data_mesgs['record']['timestamp'] as $timestamp) {
+                $trkptNode = $trksegNode->addChild('trkpt');
+                $trkptNode->addAttribute('lat', $this->pFFA->data_mesgs['record']['position_lat'][$timestamp]);
+                $trkptNode->addAttribute('lon', $this->pFFA->data_mesgs['record']['position_long'][$timestamp]);
+                $trkptNode->addChild('ele', $this->pFFA->data_mesgs['record']['altitude'][$timestamp]);
+                $trkptNode->addChild('time', date('Y-m-d\TH:i:s.000\Z', $timestamp));
+            }
+            file_put_contents($output_gpx_file, $rootNode->asXML());
+        } catch ( Exception $ex ) {
+            $this->errorMessage = $ex->getMessage();
         }
-
-        file_put_contents($output_gpx_file, $rootNode->asXML());
     }
 }
